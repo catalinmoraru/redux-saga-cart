597f008312bedecc6e70ad2994ae09dae4a7514a
catalin.moraru <catalin.moraru@visma.com>
2018-06-12 13:29:03 +0300

    first saga - simple loop

--- src/getStore.js
+++ src/getStore.js
@@ -10,7 +10,10 @@ import thunk from 'redux-thunk'
 
 import { getQuery } from './utility'
 import { reducer } from './combineReducers';
-import { defaultState } from './defaultState'
+import { defaultState } from './defaultState';
+import createSagaMiddleware from 'redux-saga';
+
+import {initSagas} from './initSagas';
 
 const stateTransformer = (state) => {
     if (Iterable.isIterable(state)) return state.toJS();
@@ -22,7 +25,8 @@ const logger = createLogger({
 });
 
 export const getStore = ()=>{
-    const middleWares = [thunk];
+    const sagaMiddleware = createSagaMiddleware();
+    const middleWares = [thunk,sagaMiddleware];
     if (getQuery()['logger']) { middleWares.push(logger)}
     const composables = [applyMiddleware(...middleWares)]
     const enhancer = compose(
@@ -33,5 +37,7 @@ export const getStore = ()=>{
         defaultState,
         enhancer
     );
+    console.info('saga middleware implemented');
+    initSagas(sagaMiddleware);
     return store;
 };
